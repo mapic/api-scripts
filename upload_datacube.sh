8f3ee55d128b174f25c9f2e157d7cbcb1ed47391
knutole <knutole@mapic.io>
2016-11-02 12:29:07 +0100

    api route for localhost

--- upload_datacube.sh
+++ upload_datacube.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 DATACUBE=$1
-node lib/upload_datacube.js $DATACUBE
+# node lib/upload_datacube.js $DATACUBE
+
+DATAPATH=$(node lib/get-data-path.js $1)
+
+echo $DATAPATH
+
+docker run -v $PWD:/sdk/ -v /:/data --env MAPIC_DOMAIN -it node:4 node /sdk/lib/upload_datacube.js $DATACUBE
