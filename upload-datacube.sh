4d43765997f48cc95685ff2b7adadf79af386040
knutole <knutole@mapic.io>
2016-11-15 17:25:31 +0100

    cleanup

--- upload-datacube.sh
+++ upload-datacube.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 DATACUBE=$1
-# node lib/upload_datacube.js $DATACUBE
-
-DATAPATH=$(node lib/get-data-path.js $1)
-
-echo $DATAPATH
+# DATAPATH=$(node lib/get-data-path.js $1)
+# echo $DATAPATH
 
 docker run -v $PWD:/sdk/ -v /:/data --env MAPIC_DOMAIN -it node:4 node /sdk/lib/upload_datacube.js $DATACUBE
