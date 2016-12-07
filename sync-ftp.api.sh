47b9783698fccb71d5e3dce0a5e5fd3116ddf193
knutole <knutole@mapic.io>
2016-12-07 15:37:44 +0100

    work in progress

--- sync-ftp.api.sh
+++ sync-ftp.api.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# docker run -v $PWD:/sdk/ -v /:/data --env MAPIC_DOMAIN -it node:4 node /sdk/lib/sync-ftp-datasets.js $1
 docker run -v $PWD:/sdk/ --rm --env MAPIC_DOMAIN -it node:4 node /sdk/lib/sync-ftp-datasets.js $1
